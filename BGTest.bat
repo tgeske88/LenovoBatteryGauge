dc695c90e2609f7f2cb308d38fff3320edbebaa3
tgeske88 <tgeske88@gmail.com>
2022-12-02 10:00:32 -0600

    removed

--- BGTest.bat
+++
@@ -1,2 +0,0 @@
-powershell -executionpolicy bypass -file "%~dp0BGT.ps1"
-C:\Windows\System32\rundll32.exe C:\ProgramData\Lenovo\ImController\Plugins\LenovoBatteryGaugePackage\x64\LenovoBatteryGaugePackage.dll,ShowBatteryGauge
